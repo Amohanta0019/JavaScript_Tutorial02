5f092e84848e67d0e4be336dccfe6041ebc7b37d
AhanaMohanta <mohantaahana444@gmail.com>
2024-04-22 21:13:29 +0530

    for loop

--- 06_Iterators/01_forloop.js
+++ 06_Iterators/01_forloop.js
@@ -1,5 +1,25 @@
-
-
+/*
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    console.log(i);
+}
+*/
+/*
+for (let i = 0; i <= 5; i++) {
+    console.log(`outer loop ${i}`);
+    for (let j = 0; j <=5 ; j++) {
+        console.log(`inner loop ${j} and inner loop value ${i}`);
+        
+    }
+}
+*/
+let p = ""
+for (let i = 1; i <= 5 ; i++) {
+    for (let j = i; j <= i; j++) {
+        p += "#";
+    }
+    console.log(p);
+}
 
 
 
