a1562f2516a845e62222377533a683229326bdde
AhanaMohanta <mohantaahana444@gmail.com>
2024-04-23 21:38:12 +0530

    array loops

---
+++ 06_Iterators/03_ArrayLoops.js
@@ -0,0 +1,35 @@
+//array specific loops
+
+//#1) for of loop:-
+const arr = [1,2,3,4,5,6]
+for (const value of arr) {
+    console.log(value);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
