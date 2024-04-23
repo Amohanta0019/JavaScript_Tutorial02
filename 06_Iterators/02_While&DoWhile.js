a382d04035c83a0d33d34e0a0153211edd498017
AhanaMohanta <mohantaahana444@gmail.com>
2024-04-23 20:34:59 +0530

    do while and while

--- 06_Iterators/02_While&DoWhile.js
+++ 06_Iterators/02_While&DoWhile.js
@@ -3,13 +3,20 @@ while (index<= 10) {
     // console.log(`value of index :- ${index}`);
     index += 2
 }
+// o.p = 0,2,4,6,8,10
 
 let myArr = ['iron-man','hulk','spider-man','thor']
 let arr = 0
 while (arr < myArr.length) {
-    // console.log(`hero number ${arr} is :- ${myArr[arr]}`);
+    console.log(`hero number ${arr} is :- ${myArr[arr]}`);
     arr += 1
 }
+//o.p:-
+// hero number 0 is :- iron-man
+// hero number 1 is :- hulk
+// hero number 2 is :- spider-man
+// hero number 3 is :- thor
+
 
 // print atleast once even before checking condition
 let score = 1
@@ -17,7 +24,7 @@ do {
     console.log(`Score is :- ${score}`);
     score++
 } while (score<=10);
-
+//o.p :- 1 to 10 
 
 
 
