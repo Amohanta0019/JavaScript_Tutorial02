ba530e578365600640429a61bca10ecf3bc866f8
AhanaMohanta <mohantaahana444@gmail.com>
2024-06-29 10:57:45 +0530

    arary basics updated

--- 02_Arrays/01_Array-basics.js
+++ 02_Arrays/01_Array-basics.js
@@ -17,22 +17,29 @@ arr.unshift(7)//[ 7, 1, 3, 4, 67, 79 ]
 arr.shift()//[ 1, 3, 4, 67, 79 ]
 // console.log(arr);
 
-// console.log(arr.includes(10));//false
-// console.log(arr.indexOf(10));//-1 means there is no element
+// console.log(arr.includes(10));//false,, if array contains 10 or not
+// console.log(arr.indexOf(10));//-1 if element not present
 
-const newArr = arr.join()
+const newArr = arr.join(":")
 // console.log(arr);//[ 1, 3, 4, 67, 79 ]
-// console.log(newArr);//1,3,4,67,79
+// console.log(newArr);//1:3:4:67:79
 // console.log(typeof newArr);//string
 
-//slice & splice
 
+
+
+
+//slice
+//=====
 console.log("a", arr);
 //last index not included & original array remains same after slicing
 const myn1 = arr.slice(1,3)
 console.log(myn1);//[ 3, 4 ]
 console.log("b", arr);//b [ 1, 3, 4, 67, 79 ]
 console.log();
+
+//splice
+//======
 //last range included & original array changed , only remaining portion included
 const myn2 = arr.splice(1,3)
 console.log(myn2); //[ 3, 4, 67 ]
