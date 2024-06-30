d5771f10a91b268363a55b8c1fd3ef66b516cf76
AhanaMohanta <mohantaahana444@gmail.com>
2024-06-30 15:16:29 +0530

    maps in js

--- 06_Iterators/05_maps.js
+++ 06_Iterators/05_maps.js
@@ -1,10 +1,17 @@
 const mynums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+
+//maps are used to access each element of array to perform some operation
 const newnums = mynums.map(
     (num) => { return num + 10 }
 )
 // console.log(newnums);
 
+
+
+
+//chaining of maps:-
+//===================
 const newnums1 = mynums.map(
     (num) => { return num * 10 } //10, 20, 30, 40,  50, 60, 70, 80, 90, 100
 ).map(
@@ -14,7 +21,7 @@ const newnums1 = mynums.map(
 )
 console.log(newnums1);
 
-
+//after each map execution initial array will be change
 
 
 
