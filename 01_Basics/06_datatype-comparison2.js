3b26340282ae19a379b3cc05f04e269ba27f2e04
AhanaMohanta <mohantaahana444@gmail.com>
2024-07-14 19:29:54 +0530

    updated

--- 01_Basics/06_datatype-comparison2.js
+++ 01_Basics/06_datatype-comparison2.js
@@ -21,10 +21,10 @@ console.log(2<=1)
 // console.log(undefined > 0);//false
 // console.log(undefined < 0);//false
 
-// === checks value + datatype
+// "==" operator compares values after performing type conversion, while the "===" operator compares values without type conversion
 
-console.log("2"===2);//false
-console.log("2"==2);//true
+console.log("2"===2);//false ,,, string != number
+console.log("2"==2);//true string("2") -> number(2) equals number(2)
 
 
 
